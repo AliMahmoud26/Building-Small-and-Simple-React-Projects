89bd3cfac87e264fa30fe29ac7767e7087bfb2de
AliMahmoud26 <alimah.engineer@gmail.com>
2026-03-19 11:38:42 +0200

    Setting up the main functionality of the App.jsx file

--- 3-grocery-bud-app/src/App.jsx
+++ 3-grocery-bud-app/src/App.jsx
@@ -1,7 +1,45 @@
+import { useState } from "react"
+import List from "./List";
+import Alert from "./Alert";
+
 function App() {
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({show:false, msg:'', type:''})
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      // display alert
+    } else if (name && isEditing) {
+      // deal with edit
+    } else {
+      // show alert
+      const newItem = {id: new Date().getTime().toString(), title: name}
+      setList(...list, newItem);
+      setName('');
+    }
+  }
   return (
     <>
-      <div>Hello World</div>
+      <section className="section-center">
+        <form className="grocery-form" onSubmit={handleSubmit}>
+          {alert.show && <Alert />}
+          <h3>Grocery Bud</h3>
+          <div className="form-control">
+            <input type="text" name="text" id="text" className="grocery" placeholder="e.g. eggs" value={name} onChange={(e) => {setName(e.target.value)}}/>
+            <button type="submit" className="submiit-btn">{isEditing ? 'Edit' : 'Submit'}</button>
+          </div>
+        </form>
+        {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} />
+          <button className="clear-btn">Clear items</button>
+        </div>
+        )}
+      </section>
     </>
   )
 }
