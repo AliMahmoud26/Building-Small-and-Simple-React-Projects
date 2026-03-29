afcf5a967d510de74bafc7ad2c28306b1c245f54
AliMahmoud26 <alimah.engineer@gmail.com>
2026-03-29 23:12:50 +0200

    Initalizing the files of the stripe submenu project

--- 5-stripe-submenu/src/App.jsx
+++ 5-stripe-submenu/src/App.jsx
@@ -1,6 +1,16 @@
+import Hero from "../../../1- Programming Courses Implementations/22- 15 React Simple Projects/react-projects-master/react-projects-master/13-stripe-submenus/final/src/Hero"
+import Navbar from "../../../1- Programming Courses Implementations/22- 15 React Simple Projects/react-projects-master/react-projects-master/13-stripe-submenus/final/src/Navbar"
+import Sidebar from "../../../1- Programming Courses Implementations/22- 15 React Simple Projects/react-projects-master/react-projects-master/13-stripe-submenus/final/src/Sidebar"
+import Submenu from "../../../1- Programming Courses Implementations/22- 15 React Simple Projects/react-projects-master/react-projects-master/13-stripe-submenus/final/src/Submenu"
+
 function App() {
   return (
-    <div>Hello</div>
+    <>
+      <Navbar />
+      <Sidebar />
+      <Hero />
+      <Submenu />
+    </>
   )
 }
 
