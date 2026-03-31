cca9556b0f3345916dbb9be0d2a7cdba8cab0063
AliMahmoud26 <alimah.engineer@gmail.com>
2026-03-31 17:35:10 +0200

    Updating context.jsx file

---
+++ 5-stripe-submenu/src/Context.jsx
@@ -0,0 +1,56 @@
+import { useState, useContext, createContext } from "react"
+import sublinks from './Data'
+const AppContext = createContext();
+
+const AppProvider = ({children}) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+    const [page, setPage] = useState({page: '', links: []});
+    const [location, setLocation] = useState({});
+
+    // A funciton to open the sidebar
+    const openSidebar = () => {
+        setIsSidebarOpen(true);
+    }
+
+    // A function to close the sidebar
+    const closeSidebar = () => {
+        setIsSidebarOpen(false);
+    }
+
+    // A function to open the submenu
+    const openSubmenu = (text, coordinates) => {
+        const page = sublinks.find((link) => link.page === text);
+        setPage(page);
+        setLocation(coordinates);
+        setIsSubmenuOpen(true);
+    }
+
+    // A function to close the submenu
+    const closeSubmenu = () => {
+        setIsSubmenuOpen(false);
+    }
+
+    return (
+        <AppContext.Provider
+        value = {{
+            isSidebarOpen,
+            openSidebar,
+            closeSidebar,
+            isSubmenuOpen,
+            openSubmenu,
+            closeSubmenu,
+            page,
+            location
+        }}
+        >
+            {children}
+        </AppContext.Provider>
+    )
+};
+
+export const useGlobalContext = () => {
+    return useContext(AppContext);
+};
+
+export {AppContext, AppProvider};
