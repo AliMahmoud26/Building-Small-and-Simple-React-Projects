0a703416b14a0aa12eb2aac7e16043f69710406f
AliMahmoud26 <alimah.engineer@gmail.com>
2026-03-31 16:13:06 +0200

    Finishing up the hero.jsx file

--- 5-stripe-submenu/src/components/Hero.jsx
+++ 5-stripe-submenu/src/components/Hero.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import phoneImg from '../assets/phone.svg';
+import { useGlobalContext } from '../context/Context';
 
 const Hero = () => {
+  const {closeSubmenu} = useGlobalContext();
+
   return (
     <section className='hero' onMouseOver={closeSubmenu}>
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payment infrastructure <br /> for the internet</h1>
+          <p>Millions of companies of all sizes—from startups to Fortune 500s—use
+            Stripe’s software and APIs to accept payments, send payouts, and
+            manage their businesses online.</p>
+          <button className="btn">Start now</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} className='phone-img' alt="phone" />
+        </article>
+      </div>
     </section>
   );
 };
