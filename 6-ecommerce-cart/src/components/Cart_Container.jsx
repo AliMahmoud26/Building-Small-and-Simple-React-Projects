02d42c6dea417d55a7ee4f93d3ed74c1ae9b575f
AliMahmoud26 <alimah.engineer@gmail.com>
2026-04-02 21:19:30 +0200

    Creating the files of the project

---
+++ 6-ecommerce-cart/src/components/Cart_Container.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cart_Container = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Cart_Container
---
+++ 6-ecommerce-cart/src/components/Cart_Item.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cart_Item = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Cart_Item
